8ae67084620af9724e9678e4487b855a0f376f36
KZ137onethe <q2274799107@126.com>
2023-03-18 20:03:27 +0800

    fix: modify username's check rules to 'account' and 'phone'

--- src/views/login/components/login-account-validate.js
+++ src/views/login/components/login-account-validate.js
@@ -1,4 +1,17 @@
-export const validateUsername = async (_rule, value) => {
+export const validateAccount = async (_rule, value) => {
+	return new Promise((resolve, reject) => {
+		if (value === '') {
+			reject(new Error('请输入账号'))
+		} else {
+			if (!/^[a-zA-Z]\w{5,19}$/.test(value)) {
+				reject(new Error('字母开头且6-20字符'))
+			}
+			resolve()
+		}
+	})
+}
+
+export const validatePhone = async (_rule, value) => {
 	return new Promise((resolve, reject) => {
 		if (value === '') {
 			reject(new Error('请输入手机号'))
@@ -9,6 +22,10 @@ export const validateUsername = async (_rule, value) => {
 			resolve()
 		}
 	})
+}
+
+export const schemaPhone = (phone) => {
+	return /^1[3-9]\d{9}$/.test(phone)
 }
 
 export const validatePassword = async (_rule, value) => {
