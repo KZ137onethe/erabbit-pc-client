944f1c521956d7bab6c1587def8762840362db3a
KZ137onethe <q2274799107@126.com>
2023-03-18 20:07:32 +0800

    fix: fix profile-related store fields and setUser mutations methods

--- src/store/module/user.js
+++ src/store/module/user.js
@@ -1,27 +1,32 @@
 // 用户模块
 const UserModule = {
-    namespaced: true,
-    state () {
-        return {
-            profile: {
-                id: '',
-                avatar: '',
-                nickname: '',
-                account: '',
-                mobile: '',
-                token: ''
-            }
-        }
-    },
-    getters: {
-    },
-    mutations: {
-        setUser (state, payload) {
-            state.profile = payload
-        }
-    },
-    actions: {
-    }
+	namespaced: true,
+	state() {
+		return {
+			profile: {
+				id: '',
+				avatar: '',
+				nickname: '',
+				birthday: '',
+				cityCode: '',
+				gender: '',
+				account: '',
+				mobile: '',
+				token: '',
+				profession: '',
+				provinceCode: '',
+			},
+		}
+	},
+	getters: {},
+	mutations: {
+		setUser(state, payload) {
+			for (let key of Object.keys(state.profile)) {
+				state.profile[key] = payload[key] ?? ''
+			}
+		},
+	},
+	actions: {},
 }
 
 export default UserModule
