e7e0174dde98bbca08780838db4b57b043241f5e
KZ137onethe <q2274799107@126.com>
2023-03-24 20:20:54 +0800

    feat: add third-party login callback route and register Route

--- src/router/index.js
+++ src/router/index.js
@@ -2,31 +2,39 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 // * 路由规则
 const routes = [
-    // 一级路由布局容器
-    {
-        path: '/',
-        component: () => import('@/views/Layout.vue'),
-        children: [
-            { path: '/', component: () => import('@/views/home/index.vue') },
-            { path: '/category/:id', component: () => import('@/views/category/index.vue') },
-            { path: '/category/sub/:id', component: () => import('@/views/category/sub.vue') },
-            { path: '/product/:id', component: () => import('@/views/goods/index.vue') }
-        ]
-    },
-    {
-        path: '/login',
-        component: () => import('@/views/login/index.vue')
-    }
+	// 一级路由布局容器
+	{
+		path: '/',
+		component: () => import('@/views/Layout.vue'),
+		children: [
+			{ path: '/', component: () => import('@/views/home/index.vue') },
+			{ path: '/category/:id', component: () => import('@/views/category/index.vue') },
+			{ path: '/category/sub/:id', component: () => import('@/views/category/sub.vue') },
+			{ path: '/product/:id', component: () => import('@/views/goods/index.vue') },
+		],
+	},
+	{
+		path: '/login',
+		component: () => import('@/views/login/index.vue'),
+	},
+	{
+		path: '/login/callback',
+		component: () => import('@/views/login/callback.vue'),
+	},
+	{
+		path: '/register',
+		component: () => import('@/views/login/register.vue'),
+	},
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes,
-    // 切换路由时，回到页面顶部
-    scrollBehavior (to, from, savedPosition) {
-        // 始终滚动到顶部
-        return { left: 0, top: 0 }
-    }
+	history: createWebHashHistory(),
+	routes,
+	// 切换路由时，回到页面顶部
+	scrollBehavior(to, from, savedPosition) {
+		// 始终滚动到顶部
+		return { left: 0, top: 0 }
+	},
 })
 
 export default router
