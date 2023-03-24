cf7d954e50a56bb54d8545d8cb481a0feffee63a
KZ137onethe <q2274799107@126.com>
2023-03-24 20:17:43 +0800

    feat: add encapsulated mutations function for 'vuex'

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,11 +1,9 @@
 import { useState } from './useState'
 import { useGetters } from './useGetters'
+import { useMutations } from './useMutations'
 import { useActions } from './useActions'
 import { useLazyData } from './useLazyData'
 
-export {
-  useState,
-  useGetters,
-  useActions,
-  useLazyData
-}
+// ! tips: 封装的vuex函数不能在嵌套中使用！！！
+
+export { useState, useGetters, useMutations, useActions, useLazyData }
