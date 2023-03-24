98f28c405b02355489358aa5470012cb94a9b2a8
KZ137onethe <q2274799107@126.com>
2023-03-24 20:13:45 +0800

    feat:add QQ login interfaces

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,18 @@ import { topCategory } from './contants'
 import { _findAllCategory, _findTopCategory, _findSubCategoryFilter, _findSubCategoryGoods } from './category'
 import { _findBrand, _findBanner, _findFreshThing, _findHot, _findGoodsColumn, _findSpecial } from './home'
 import { _findGoodsDetail, _findGoodsRelevant, _findGoodsHot, _findGoodsCommentInfo, _findGoodsCommentData } from './product.js'
-import { _userAccountLogin,_userSMSLogin,_userGetVerificationCode } from './user'
+import {
+	_userAccountLogin,
+	_userSMSLogin,
+	_userGetVerificationCode,
+	_userQQLogin,
+	_userQQBindGetVerificationCode,
+	_userQQBindPhone,
+	_userCheckAccount,
+	_userQQPatchAccount,
+	_userPCRegisterVerificationCode,
+	_userPCRegister,
+} from './user'
 
 export {
 	request,
@@ -29,5 +40,12 @@ export {
 	// 用户相关
 	_userAccountLogin,
 	_userSMSLogin,
-	_userGetVerificationCode
+	_userGetVerificationCode,
+	_userQQLogin,
+	_userQQBindGetVerificationCode,
+	_userQQBindPhone,
+	_userCheckAccount,
+	_userQQPatchAccount,
+	_userPCRegisterVerificationCode,
+	_userPCRegister,
 }
--- src/api/user.js
+++ src/api/user.js
@@ -5,8 +5,17 @@ import { request } from './request'
  * @param {String} account -- 用户账号
  * @param {String} password -- 用户密码
  */
-const _userAccountLogin = ({ account, password }) => {
+export const _userAccountLogin = ({ account, password }) => {
 	return request('/login', 'post', { account, password })
+}
+
+/**
+ * ? 发送手机的短信验证码
+ * @param {Number} phone -- 手机号
+ * @returns Object
+ */
+export const _userGetVerificationCode = ({ phone }) => {
+	return request('/login/code', 'get', { mobile: phone })
 }
 
 /**
@@ -15,17 +24,76 @@ const _userAccountLogin = ({ account, password }) => {
  * @param {Number} verificationCode -- 验证码
  * @returns Object
  */
-const _userSMSLogin = ({ phone, verificationCode }) => {
+export const _userSMSLogin = ({ phone, verificationCode }) => {
 	return request('/login/code', 'post', { mobile: phone, code: verificationCode })
 }
 
 /**
- * ? 发送短信验证码
- * @param {Number} phone -- 手机号
+ * 第三方登录
+ * @param {*} unionId -- QQ的唯一标识
+ * @param {*} source -- 注册来源，1为pc，2为webapp，3为微信小程序，4为Android，5为ios,6为qq,7为微信
  * @returns Object
  */
-const _userGetVerificationCode = ({ phone }) => {
-	return request('/login/code', 'get', { mobile: phone })
+export const _userQQLogin = ({ unionId, source = 6 }) => {
+	return request('/login/social', 'post', { unionId, source })
 }
 
-export { _userAccountLogin, _userSMSLogin, _userGetVerificationCode }
+/**
+ * 第三方QQ登录时，绑定手机号，发送验证码
+ * @param {Number} phone -- 手机号
+ * @returns
+ */
+export const _userQQBindGetVerificationCode = (phone) => {
+	return request('/login/social/code', 'get', { mobile: phone })
+}
+
+/**
+ * 第三方QQ登录时，绑定手机号
+ * @param { String } unionId -- 第三方登录QQ唯一标识
+ * @param { Number } mobile -- 手机号
+ * @param { Number } verificationCode -- 验证码
+ * @returns
+ */
+export const _userQQBindPhone = ({ unionId, mobile, verificationCode }) => {
+	return request('/login/social/bind', 'post', { unionId, mobile, code: verificationCode })
+}
+
+/**
+ * 检查用户账号是否唯一
+ * @param {string} account -- 用户账号名
+ * @returns
+ */
+export const _userCheckAccount = (account) => {
+	return request('/register/check', 'get', { account })
+}
+
+/**
+ * 第三方登录，完善用户信息接口 => 没有官网账号
+ * @param {string} unionId  -- QQ的唯一标识
+ * @param {Object} { account, phone, verificationCode, password } -- 用户账号名,用户手机号,手机验证码,账号密码
+ * @returns
+ */
+export const _userQQPatchAccount = (unionId, { account, phone, verificationCode, password }) => {
+	return request(`/login/social/${unionId}/complement`, 'post', { account, mobile: phone, code: verificationCode, password })
+}
+
+/**
+ * PC端，用户注册接口时，获取手机验证码
+ * @param {*} phone -- 手机号
+ * @returns
+ */
+export const _userPCRegisterVerificationCode = (phone) => {
+	return request('/register/code', 'get', { mobile: phone })
+}
+
+/**
+ * PC端，用户注册接口
+ * @param {} account -- 用户账号名
+ * @param {} phone -- 手机号
+ * @param {} verificationCode -- 验证码
+ * @param {} password -- 用户账号密码
+ * @returns
+ */
+export const _userPCRegister = ({ account, phone, verificationCode, password }) => {
+	return request('/register', 'post', { account, mobile: phone, code: verificationCode, password })
+}
