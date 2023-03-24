53dc9b0d164c23f5b6e7af39f9992ffdb1c1a841
KZ137onethe <q2274799107@126.com>
2023-03-24 20:30:11 +0800

    feat: add register page

---
+++ src/views/login/register.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="login-register">
+		<LoginHeader>
+			<template #subTitle>账号注册</template>
+		</LoginHeader>
+		<RegisterPanel></RegisterPanel>
+		<LoginFooter></LoginFooter>
+	</div>
+</template>
+
+<script>
+import LoginHeader from './components/login-header.vue'
+import LoginFooter from './components/login-footer.vue'
+import RegisterPanel from './components/register-panel.vue'
+
+export default {
+	setup() {
+		return {}
+	},
+	components: { LoginHeader, RegisterPanel, LoginFooter },
+}
+</script>
+
+<style lang="less" scoped></style>
