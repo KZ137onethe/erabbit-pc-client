e9a8176a2827b0eb121931dc4bbd6573961fe8f4
KZ137onethe <q2274799107@126.com>
2023-03-24 20:11:08 +0800

    feat: vite add third party QQ login

--- vite.config.js
+++ vite.config.js
@@ -8,30 +8,40 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [AntDesignVueResolver()]
-    })
-  ],
-  css: {
-    preprocessorOptions: {
-      less: {
-        // 自动导入 全局的less文件
-        additionalData: `
+	plugins: [
+		vue(),
+		Components({
+			resolvers: [AntDesignVueResolver()],
+		}),
+	],
+	css: {
+		preprocessorOptions: {
+			less: {
+				// 自动导入 全局的less文件
+				additionalData: `
           @import "./src/assets/styles/variables.less";
           @import "./src/assets/styles/mixin.less";
           @import "./src/assets/styles/effect.less";
         `,
-        javascriptEnabled: true,
-      },
-    },
-  },
-  resolve: {
-    // 使用别名'@'替代根目录
-    alias: {
-      '@': path.resolve( __dirname, './src' )
-    },
-    extensions: ['.js', '.vue', '.json', '.ts', '.tsx', '.jsx', '.mjs', '.wasm']
-  }
+				javascriptEnabled: true,
+			},
+		},
+	},
+	resolve: {
+		// 使用别名'@'替代根目录
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+		extensions: ['.js', '.vue', '.json', '.ts', '.tsx', '.jsx', '.mjs', '.wasm'],
+	},
+	server: {
+		port: 8080,
+	},
+	build: {
+		rollupOptions: {
+			external: {
+				qc: 'QC',
+			},
+		},
+	},
 })
