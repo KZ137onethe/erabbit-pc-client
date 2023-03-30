f039b7562404a658fcca0163609b14960950dca2
KZ137onethe <q2274799107@126.com>
2023-03-30 23:13:06 +0800

    style:format code

--- src/views/goods/index.js
+++ src/views/goods/index.js
@@ -4,17 +4,21 @@ const goods = ref(null)
 
 // 获取商品详情
 export const useGoods = (route) => {
-  watch(() => route.params.id, (newVal) => {
-    if (newVal && `/product/${newVal}` === route.path) {
-      _findGoodsDetail(route.params.id).then(data => {
-        // 让商品数据为null让后使用v-if的组件可以重新销毁和创建
-        goods.value = null
-        nextTick(() => {
-          goods.value = data.result
-          // console.log('goods: ', goods.value)
-        })
-      })
-    }
-  }, { immediate: true })
-  return goods
+	watch(
+		() => route.params.id,
+		(newVal) => {
+			if (newVal && `/product/${newVal}` === route.path) {
+				_findGoodsDetail(route.params.id).then((data) => {
+					// 让商品数据为null让后使用v-if的组件可以重新销毁和创建
+					goods.value = null
+					nextTick(() => {
+						goods.value = data.result
+						// console.log('goods: ', goods.value)
+					})
+				})
+			}
+		},
+		{ immediate: true }
+	)
+	return goods
 }
