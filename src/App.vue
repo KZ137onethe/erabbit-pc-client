9bb9ee0559b85cbd66c5f87aa94f7ff0d7e31846
KZ137onethe <q2274799107@126.com>
2023-04-09 18:45:19 +0800

    style: 格式化代码

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
-  <router-view />
+	<router-view />
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  setup () {
-  }
-
+	name: 'App',
+	setup() {},
 }
 </script>
 <style lang="less"></style>
