cf935979856c5fc3fa510081e489baf229a718a1
KZ137onethe <q2274799107@126.com>
2023-05-03 23:26:08 +0800

    feat: 添加全局的加载页面

---
+++ src/components/library/xtx-loading.vue
@@ -0,0 +1,32 @@
+<template>
+	<a-spin :indicator="indicator" />
+</template>
+<script>
+import { LoadingOutlined } from '@ant-design/icons-vue'
+import { defineComponent, h } from 'vue'
+export default defineComponent({
+	props: {
+		size: {
+			type: Number,
+			default: 24,
+		},
+	},
+	setup(props) {
+		const indicator = h(LoadingOutlined, {
+			style: {
+				fontSize: props.size + 'px',
+			},
+			spin: true,
+		})
+		return {
+			indicator,
+		}
+	},
+})
+</script>
+
+<style lang="less" scoped>
+:deep(svg) {
+	fill: @xtxColor;
+}
+</style>
