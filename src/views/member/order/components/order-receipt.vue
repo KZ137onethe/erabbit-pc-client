cfd996c22ed2ddc68917fea9d21d3838d80d1935
KZ137onethe <q2274799107@126.com>
2023-05-04 23:24:41 +0800

    feat: 新增确认收货

--- src/views/member/order/components/order-receipt.vue
+++ src/views/member/order/components/order-receipt.vue
@@ -1,15 +1,43 @@
 <template>
-	<OperationButton title="确认收货" @btnClick="click" class="order-receipt"></OperationButton>
+	<OperationButton title="确认收货" @btnClick="showModal" class="order-receipt">
+		<a-modal v-model:visible="visible" title="确认收货" @ok="handleOk">
+			<p class="description">您确认收到货吗？确认后货款将会打给卖家。</p>
+		</a-modal>
+	</OperationButton>
 </template>
 
 <script>
-import OperationButton from './operation-btn.vue'
+import { ref } from 'vue'
+import { memberApi } from '@/api'
 
+import OperationButton from './operation-btn.vue'
+import { message } from 'ant-design-vue'
+import 'ant-design-vue/es/message/style/css'
+
+const { _receiptOrder } = memberApi
 export default {
-	setup() {
-		const click = () => console.log('hhh')
+	props: {
+		orderId: {
+			type: String,
+			required: true,
+		},
+	},
+	setup(props, { emit }) {
+		const visible = ref(false)
+		const showModal = () => {
+			visible.value = true
+		}
+		const handleOk = () => {
+			_receiptOrder(props.orderId).then(() => {
+				message('确认收货成功!')
+				visible.value = false
+				emit('refresh')
+			})
+		}
 		return {
-			click,
+			visible,
+			showModal,
+			handleOk,
 		}
 	},
 	components: {
