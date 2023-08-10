915c536c063fc0fc445c95ee123db6519657f061
guanhai <q2274799107@126.com>
2023-08-10 20:56:57 +0800

    chore(add validaterules): modify and add verification rules for prettier and eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,16 +6,16 @@ module.exports = {
 		node: true,
 		es2021: true,
 	},
-	/* global variable */
+	// global variable
 	globals: {},
 	/* 所配置的解析器 */
-	parser: '@babel/eslint-parser',
+	parser: "@babel/eslint-parser",
 	parserOptions: {
 		ecmaVersion: 6,
-		sourceType: 'module',
+		sourceType: "module",
 		requireConfigFile: false,
 		babelOptions: {
-			presets: ['@babel/preset-env'],
+			presets: ["@babel/preset-env"],
 		},
 		ecmaFeatures: {
 			// 是否启用严格模式
@@ -25,21 +25,55 @@ module.exports = {
 		},
 	},
 	extends: [
-		'eslint-config-prettier',
-		'eslint:recommended', // 使用推荐的eslint
-		'plugin:vue/vue3-recommended', // 使用插件支持vue3
-		'plugin:vue/vue3-essential',
+		"eslint-config-prettier",
+		"eslint:recommended", // 使用推荐的eslint
+		"plugin:vue/vue3-recommended", // 使用插件支持vue3
+		"plugin:vue/vue3-essential",
 		// 1.继承.prettierrc.js文件规则  2.开启rules的 "prettier/prettier": "error"  3.eslint fix的同时执行prettier格式化
-		'plugin:prettier/recommended',
+		"plugin:prettier/recommended",
 	],
 	overrides: [],
 	/* 插件 */
-	plugins: ['vue', 'import'],
+	plugins: ["vue", "import"],
 	/* 规则 */
 	rules: {
-		// 生产模式不允许使用log
-		'no-console': process.env.NODE_ENV === 'production' ? ['error', { allow: ['error', 'warn'] }] : 'off',
+		"no-console": "off",
+		"no-continue": "off",
+		"no-restricted-syntax": "off",
+		"no-plusplus": [ "error",
+			{
+				"allowForLoopAfterthoughts": true
+			}
+		],
+		// 是否限制在函数体内对函数形参赋值
+		"no-param-reassign": "off",
+		"no-shadow": "off",
+		"guard-for-in": "off",
+		"prettier/prettier": "error",
+
+		// 是否对导入语句中文件扩展名的验证。
+		"import/extensions": "off",
+		// 是否开启对解析导入路径的验证
+		"import/no-unresolved": "off",
+		// 检查项目中是否存在未声明为依赖的外部模块导入
+		"import/no-extraneous-dependencies": "off",
+		// 用于推荐优先使用默认导出（default export）而不是命名导出（named export）
+		"import/prefer-default-export": "off",
+		// 用于强制导入语句放在非导入语句之前
+		"import/first": "warn", // https://github.com/vuejs/vue-eslint-parser/issues/58
+		"vue/first-attribute-linebreak": 0,
 		// 单文件组件是否已多个词汇命名
-		'vue/multi-word-component-names': 'off',
+		"vue/multi-word-component-names": "off",
+		// 当遇到对尚未声明的标识符的引用时，该规则将发出警告。
+		"no-use-before-define": [ "error",
+			{
+        "functions": false,
+        "classes": true,
+        "variables": true,
+        "allowNamedExports": false
+			}
+		],
+		// 因为AxiosCancel必须实例化而能静态化所以加的规则，如果有办法解决可以取消
+		"class-methods-use-this": "off",
 	},
 }
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -13,28 +13,26 @@ module.exports = {
     // 在语句末尾是否需要分号
     semi: false,
     // 是否使用单引号
-    singleQuote: true,
-    // 更改引用对象属性的时间 可选值"<as-needed|consistent|preserve>"
+    singleQuote: false,
+    // jsx中使用单引号
+    jsxSingleQuote: true,
+    // 仅在必需时为对象的key添加引号
     quoteProps: 'as-needed',
-    // 在JSX中使用单引号而不是双引号
-    jsxSingleQuote: false,
     // 多行时尽可能打印尾随逗号。（例如，单行数组永远不会出现逗号结尾。） 可选值"<none|es5|all>"，默认none
-    trailingComma: 'es5',
-    // 在对象文字中的括号之间打印空格
+    trailingComma: 'all',
+// 在对象前后添加空格-eg: { foo: bar }
+
     bracketSpacing: true,
-    // jsx 标签的反尖括号需要换行
-    jsxBracketSameLine: false,
+    // 将多行HTML（HTML、JSX、Vue、Angular）元素的>单独放在下一行（不适用于自关闭元素）
+    bracketLine: false,
     // 在单独的箭头函数参数周围包括括号 always：(x) => x \ avoid：x => x
     arrowParens: 'always',
-    // 这两个选项可用于格式化以给定字符偏移量（分别包括和不包括）开始和结束的代码
-    rangeStart: 0,
-    rangeEnd: Infinity,
-    // 指定要使用的解析器，不需要写文件开头的 @prettier
+// 是否限制为仅格式化在文件顶部包含特殊注释（称为杂注）的文件
+
     requirePragma: false,
-    // 不需要自动在文件开头插入 @prettier
     insertPragma: false,
-    // 使用默认的折行标准 always\never\preserve
-    proseWrap: 'preserve',
+    //  如果超过打印宽度，则换行。
+    proseWrap: 'always',
     // 指定HTML文件的全局空格敏感度 css\strict\ignore
     htmlWhitespaceSensitivity: 'css',
     // Vue文件脚本和样式标签缩进
@@ -43,5 +41,7 @@ module.exports = {
     //然而在 Linux 和 Unix 中只使用简单的换行分隔符 (LF)。
     //对应的控制字符为 "\n" (LF) 和 "\r\n"(CRLF)。auto意为保持现有的行尾
     // 换行符使用 lf 结尾是 可选值"<auto|lf|crlf|cr>"
-    endOfLine: 'auto',
+    endOfLine: 'lf',
+    //对引用代码进行格式化
+    embeddedLanguageFormatting: "auto",
 };
